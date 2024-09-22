d9c08bd610413110015abe764cf2cb37cf475a64
Mitesh2001 <miteshladva.dgrs@gmail.com>
2024-09-22 20:27:48 +0530

    removed server action from the slug page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,7 +2,6 @@
 import { notFound } from 'next/navigation';
 import NoteArea from '../components/NoteArea';
 import { NextPage } from 'next';
-import { fetchNoteBySlug } from '../lib/actions';
 
 interface SlugPageProps {
     params: {
@@ -13,15 +12,13 @@ interface SlugPageProps {
 const Page: NextPage<SlugPageProps> = async ({ params }) => {
     const { slug } = params;
 
-    const note = await fetchNoteBySlug(slug);
-
     if (!slug) {
         notFound();
     }
 
     return (
         <div>
-            <NoteArea {...{ slug }} defaultContent={note?.content} />
+            <NoteArea {...{ slug }} defaultContent={""} />
         </div>
     );
 }
