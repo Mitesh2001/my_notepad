6e3dc799d5240efe039e89b646c14e010bb9a6b2
Ladva Mitesh <60682333+Mitesh2001@users.noreply.github.com>
2024-10-02 19:16:22 +0000

    Change the text area to resemble the experience and make the 'Create … (#2) (#3)
    * Change the text area to resemble the experience and make the 'Create New Note' button functional.
    
    * Fixed the es lint errors

--- app/utils/generateSlug.ts
+++
@@ -1,9 +0,0 @@
-export const generateRandomSlug = (length: number = 6): string => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let slug = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        slug += characters[randomIndex];
-    }
-    return slug;
-}
