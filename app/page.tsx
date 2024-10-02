b23495d2a776201f0f8689074aeb212451d981f3
Mitesh2001 <miteshladva.dgrs@gmail.com>
2024-10-02 11:18:17 +0530

    export home page with force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-'use client';
+import { redirect } from "next/navigation";
+import { generateRandomSlug } from "./utils/generateSlug";
 
-// import { useEffect } from 'react';
-// import { useRouter } from 'next/navigation';
-// import { generateRandomSlug } from './utils/generateSlug';
+export const dynamic = 'force-dynamic';
 
-export default function HomePage() {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const randomSlug = generateRandomSlug();
-  //   console.log("randomSlug", randomSlug);
-  //   // Redirect to the generated slug
-  //   router.push(`/${randomSlug}`);
-  // }, [router]);
-
-  // Optionally, you can return a loading state while the redirect happens
-  return <div>Home Page</div>;
+const HomePage = async () => {
+  const randomSlug = generateRandomSlug();
+  redirect(randomSlug);
 }
+
+export default HomePage;
