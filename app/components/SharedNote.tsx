121004b9b5e08abc89614692a4ce698ac23c4e50
Ladva Mitesh <60682333+Mitesh2001@users.noreply.github.com>
2024-10-12 19:07:59 +0000

    Develop to main (#11)
    * Change the text area to resemble the experience and make the 'Create … (#2)
    
    * Change the text area to resemble the experience and make the 'Create New Note' button functional.
    
    * Fixed the es lint errors
    
    * Add feature to copy editable link to clipboard (#4)
    
    * Feature/shared link (#6)
    
    * created dynamic route page for sharedSlug
    
    * extends NoteModel with sharedSlug
    
    * created server actions to add or modify the value of sharedSlug
    
    * created the component to show shared content in readOnly mode
    
    * Implemented the logic to generate the sharedSlug and copy the shared URL link to clipboard
    
    * Feature/shared link (#8)
    
    * created dynamic route page for sharedSlug
    
    * extends NoteModel with sharedSlug
    
    * created server actions to add or modify the value of sharedSlug
    
    * created the component to show shared content in readOnly mode
    
    * Implemented the logic to generate the sharedSlug and copy the shared URL link to clipboard
    
    * - installed heroicons/react package and show clipboard icon (#10)
    
    - Implemented functionality of copy content to clipboard on click of clipboard icon

--- app/components/SharedNote.tsx
+++ app/components/SharedNote.tsx
@@ -3,6 +3,7 @@
 import React, { FC, useState } from 'react';
 import { Counts, Note } from '../lib/types';
 import { getCharCount, getWordCount } from '../lib/helper';
+import { ClipboardIcon } from '@heroicons/react/24/outline'
 
 interface SharedNoteProps {
     content: Note['content'];
@@ -12,10 +13,21 @@ const SharedNote: FC<SharedNoteProps> = ({ content }) => {
 
     const [counts] = useState<Counts>({ word: getWordCount(content), char: getCharCount(content) });
 
+    const copyToClipboard = async () => {
+        await navigator.clipboard.writeText(content).then(() => {
+            alert('Copied to clipboard');
+        });
+    }
+
     return (
         <>
             <div className="flex justify-between items-center pb-4 border-b">
                 <h1 className="text-xl font-semibold text-blue-600">Clip Note </h1>
+                <div className="flex space-x-10">
+                    <button onClick={copyToClipboard} className="text-gray-500 hover:text-gray-800 text-2xl">
+                        <ClipboardIcon className='h-5' />
+                    </button>
+                </div>
             </div>
 
             <div className="w-full h-96 py-4 rounded-md text-lg bg-white">
