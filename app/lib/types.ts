3c55df7822027f59218202642a94d49f21851221
Mitesh2001 <miteshladva.dgrs@gmail.com>
2024-10-12 23:56:22 +0530

    extends NoteModel with sharedSlug

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -9,13 +9,15 @@ export interface Note {
     _id: Types.ObjectId;
     slug: string;
     content: string;
+    sharedSlug: string | null;
 };
 
 export type NoteModelType = Model<Note>;
 
 const noteSchema = new Schema<Note, NoteModelType>({
     slug: { type: String, require: true, unique: true },
-    content: { type: String, require: false, default: "" }
+    content: { type: String, require: false, default: "" },
+    sharedSlug: { type: String, require: false, unique: true, default: null },
 })
 
 export const NoteModel: NoteModelType = models.Note || model('Note', noteSchema);
